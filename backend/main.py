c5006a420dd321b043eef102ab20a54b834ba652
KushagraTyagi26 <kushagratyagi840@gmail.com>
2026-08-27 23:07:28 +0530

    Allow production frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -31,6 +31,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+         "https://true-estate.vercel.app",
+         
     ],
     allow_credentials=True,
     allow_methods=["*"],
